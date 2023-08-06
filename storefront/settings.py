392467d7cd2a482c529d04724e5aa20b7734522f
Paw565pl <paw565pl@gmail.com>
2023-08-06 23:53:01 +0200

    changed db to postgresql

--- storefront/settings.py
+++ storefront/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "storefront",
+        "USER": "postgres",
+        "PASSWORD": "mysecretpassword",
+        "HOST": "0.0.0.0",
+        "PORT": "5432",
     }
 }
 
