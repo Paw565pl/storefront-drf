b994905126007aa3e06c8c5b0fc8d663588f9369
Paw565pl <paw565pl@gmail.com>
2023-08-11 23:07:21 +0200

    created custom canceling order permission

--- store/models.py
+++ store/models.py
@@ -84,6 +84,11 @@ class Order(models.Model):
     )
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    class Meta:
+        permissions = [
+            ("cancel_order", "Can cancel order"),
+        ]
+
 
 class Address(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
