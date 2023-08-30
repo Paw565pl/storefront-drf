e5319edc56886da5a4167a441cfeede679c8607a
Paw565pl <paw565pl@gmail.com>
2023-08-30 13:13:10 +0200

    add more scenarios to locust

--- locust/browse_products.py
+++ locust/browse_products.py
@@ -1,11 +1,11 @@
 from random import randint
-from locust import HttpUser, task, between
+from locust import FastHttpUser, task, between
 
 
-class WebsiteUser(HttpUser):
+class WebsiteUser(FastHttpUser):
     wait_time = between(1, 10)
 
-    @task(2)
+    @task(5)
     def view_products(self):
         collection_id = randint(2, 10)
         self.client.get(
@@ -17,19 +17,23 @@ class WebsiteUser(HttpUser):
         product_id = randint(1, 1000)
         self.client.get(f"/store/products/{product_id}/", name="/store/products/:id/")
 
-    @task(1)
+    @task(2)
     def add_to_cart(self):
         product_id = randint(1, 100)
 
         self.client.post(
             f"/store/carts/{self.cart_id}/items/",
             name="store/carts/:cart_id/items/",
-            json={"product_id": product_id, "quantity": product_id // 2},
+            json={"product_id": product_id, "quantity": product_id // randint(1, 5)},
         )
 
-    @task(5)
+    @task(3)
+    def main_endpoint(self):
+        self.client.get("", name="/")
+
+    @task(1)
     def slow_endpoint(self):
-        self.client.get("")
+        self.client.get("/playground/slow/", name="/playground/slow/")
 
     def on_start(self):
         response = self.client.post("/store/carts/")
