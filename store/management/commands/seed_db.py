46f7bc9064c03483394cd76eb97b408838267542
Paw565pl <paw565pl@gmail.com>
2024-03-23 19:13:56 +0100

    clean up commands files

--- store/management/commands/seed_db.py
+++ store/management/commands/seed_db.py
@@ -1,5 +1,6 @@
-from typing import Any
 from pathlib import Path
+from typing import Any
+
 from django.core.management.base import BaseCommand
 from django.db import connection
 
@@ -7,13 +8,13 @@ from django.db import connection
 class Command(BaseCommand):
     help = "Populates database with collections and products."
 
-    def handle(self, *args: Any, **options: Any) -> str | None:
-        self.stdout.write("Populating database...", ending="\n")
+    def handle(self, *args: Any, **options: Any):
+        self.stdout.write("Populating database...")
         sql = Path("./sql/seed_store.sql").read_text()
 
         with connection.cursor() as cursor:
             try:
                 cursor.execute(sql)
-                self.stdout.write("Done!", ending="")
-            except Exception as e:
-                self.stdout.write("Data has been already been seeded!", ending="")
+                self.stdout.write("Done!")
+            except Exception:
+                self.stdout.write("Data has been already seeded!")
