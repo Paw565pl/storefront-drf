f9379ce3ca8fafa146c89362d6145a0ff0a4e715
Paw565pl <paw565pl@gmail.com>
2024-03-30 17:50:55 +0100

    test(products): create fixture for product image

--- products/tests/conftest.py
+++ products/tests/conftest.py
@@ -1,17 +1,22 @@
 import pytest
 from model_bakery import baker
 
-from products.models import Collection, Product
-
-
-@pytest.fixture
-def create_product():
-    collection = baker.make(Collection)
-    product = baker.make(Product, collection=collection)
-    return product
+from products.models import Collection, Product, ProductImage
 
 
 @pytest.fixture
 def create_collection():
     collection = baker.make(Collection)
     return collection
+
+
+@pytest.fixture
+def create_product(create_collection):
+    product = baker.make(Product, collection=create_collection)
+    return product
+
+
+@pytest.fixture
+def create_product_image(create_product):
+    image = baker.make(ProductImage, product=create_product, _create_files=True)
+    return image
