8e32159372a2bc00f140c46b40cc94a8e9c97fac
Paw565pl <paw565pl@gmail.com>
2024-03-30 18:07:35 +0100

    chore(core): cleanup user admin

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.contenttypes.admin import GenericTabularInline
-from store.admin import ProductAdmin, ProductImageInline
-from store.models import Product
-from tags.models import TaggedItem
+
 from .models import User
 
 
@@ -27,17 +24,3 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     )
-
-
-class TagInline(GenericTabularInline):
-    model = TaggedItem
-    autocomplete_fields = ["tag"]
-    extra = 1
-
-
-class CustomProductAdmin(ProductAdmin):
-    inlines = [TagInline, ProductImageInline]
-
-
-admin.site.unregister(Product)
-admin.site.register(Product, CustomProductAdmin)
