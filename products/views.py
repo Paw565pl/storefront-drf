c1c31197ab067dedf60bbe2409eca657e001d109
Paw565pl <paw565pl@gmail.com>
2024-03-30 12:37:37 +0100

    feat(products): create product image view set

--- products/views.py
+++ products/views.py
@@ -1,13 +1,14 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_cookie
+from rest_framework.generics import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 
 from products.filters import ProductFilter
 from products.mixins import MultipleFieldLookupMixin
-from products.models import Product
+from products.models import Product, ProductImage
 from products.permissions import IsAdminOrReadOnly
-from products.serializers import ProductSerializer
+from products.serializers import ProductSerializer, ProductImageSerializer
 
 
 # Create your views here.
@@ -27,3 +28,18 @@ class ProductViewSet(MultipleFieldLookupMixin, ModelViewSet):
     @method_decorator([cache_page(60 * 5), vary_on_cookie])
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
+
+
+class ProductImageViewSet(ModelViewSet):
+    serializer_class = ProductImageSerializer
+    ordering_fields = ["id"]
+
+    def get_queryset(self):
+        product_id = self.kwargs["product_pk"]
+        get_object_or_404(Product, pk=product_id)
+        return ProductImage.objects.filter(product=self.kwargs["product_pk"]).order_by("id")
+
+    def create(self, request, *args, **kwargs):
+        product_id = self.kwargs["product_pk"]
+        get_object_or_404(Product, pk=product_id)
+        return super().create(request, *args, **kwargs)
