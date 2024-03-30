26eba7f1e87b1b323817b025041cc2da7cabb65c
Paw565pl <paw565pl@gmail.com>
2024-03-30 11:19:20 +0100

    chore: disable cache for test settings

--- storefront/settings/test.py
+++ storefront/settings/test.py
@@ -8,15 +8,6 @@ SECRET_KEY = "django-insecure-p84^d=98%mj4bvtgo7dn3d38djc*1cfm-!bb9=+@4@1m+xnbz0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# DEBUG_TOOLBAR_CONFIG = {
-#     "SHOW_TOOLBAR_CALLBACK": lambda request: True,
-# }
-#
-# INSTALLED_APPS.append("debug_toolbar")
-# MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
-#
-# INTERNAL_IPS = ["127.0.0.1"]
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -25,15 +16,5 @@ DATABASES = {
         "PASSWORD": "mysecretpassword",
         "HOST": environ.get("DB_HOST", "localhost"),
         "PORT": "5432",
-    }
-}
-
-CELERY_BROKER_URL = "redis://redis:6379"
-
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://redis:6379",
-        "TIMEOUT": 10 * 60,
     }
 }
