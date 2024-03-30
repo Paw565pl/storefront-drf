eb5465c3ad4d657c50c3c845bf11a46ebcc246b6
Paw565pl <paw565pl@gmail.com>
2024-03-30 12:38:23 +0100

    feat(products): create nested router for product image view set

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,15 @@
-from rest_framework.routers import SimpleRouter
+from django.urls import path, include
+from rest_framework_nested import routers
 
-from products.views import ProductViewSet
+from products.views import ProductViewSet, ProductImageViewSet
 
-router = SimpleRouter()
+router = routers.SimpleRouter()
 router.register(r"products", ProductViewSet, basename="products")
 
-urlpatterns = router.urls
+product_router = routers.NestedSimpleRouter(router, r"products", lookup="product")
+product_router.register(r"images", ProductImageViewSet, basename="product-images")
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("", include(product_router.urls)),
+]
