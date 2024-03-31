8959b0fa36fb6b70ff965e210eaf9a73086bb673
Paw565pl <paw565pl@gmail.com>
2024-03-31 15:00:25 +0200

    feat(products): create url for collection view set

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 
-from products.views import ProductViewSet, ProductImageViewSet, ProductReviewViewSet
+from products.views import (
+    ProductViewSet,
+    ProductImageViewSet,
+    ProductReviewViewSet,
+    CollectionViewSet,
+)
 
 router = routers.SimpleRouter()
 router.register(r"products", ProductViewSet, basename="products")
+router.register(r"collections", CollectionViewSet, basename="collections")
 
 product_router = routers.NestedSimpleRouter(router, r"products", lookup="product")
 product_router.register(r"images", ProductImageViewSet, basename="product-images")
