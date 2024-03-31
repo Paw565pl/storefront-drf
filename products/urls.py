4a279741313a178260bf27497281f52440bf6bd6
Paw565pl <paw565pl@gmail.com>
2024-03-31 13:34:08 +0200

    feat(products): create url for product review view set

--- products/urls.py
+++ products/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 
-from products.views import ProductViewSet, ProductImageViewSet
+from products.views import ProductViewSet, ProductImageViewSet, ProductReviewViewSet
 
 router = routers.SimpleRouter()
 router.register(r"products", ProductViewSet, basename="products")
 
 product_router = routers.NestedSimpleRouter(router, r"products", lookup="product")
 product_router.register(r"images", ProductImageViewSet, basename="product-images")
+product_router.register(r"reviews", ProductReviewViewSet, basename="product-reviews")
 
 urlpatterns = [
     path("", include(router.urls)),
