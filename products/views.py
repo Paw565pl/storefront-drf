421e60eeef80b86998c72c46edd09d02473fad46
Paw565pl <paw565pl@gmail.com>
2024-03-31 13:33:12 +0200

    feat(products): create product review view set

--- products/views.py
+++ products/views.py
@@ -1,13 +1,18 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from django.views.decorators.vary import vary_on_cookie
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
-from products.filters import ProductFilter
+from products.filters import ProductFilter, ReviewFilter
 from products.mixins import MultipleFieldLookupMixin
-from products.models import Product, ProductImage
-from products.permissions import IsAdminOrReadOnly
-from products.serializers import ProductSerializer, ProductImageSerializer
+from products.models import Product, ProductImage, Review
+from products.permissions import IsAdminOrReadOnly, IsAuthorOrReadOnly
+from products.serializers import (
+    ProductSerializer,
+    ProductImageSerializer,
+    ReviewSerializer,
+)
 from products.utils import get_product_or_404
 
 
@@ -33,12 +38,29 @@ class ProductViewSet(MultipleFieldLookupMixin, ModelViewSet):
 class ProductImageViewSet(ModelViewSet):
     serializer_class = ProductImageSerializer
     permission_classes = [IsAdminOrReadOnly]
-    ordering_fields = ["id"]
+    ordering_fields = []
 
     def get_queryset(self):
         product_identifier = self.kwargs["product_pk"]
         product = get_product_or_404(product_identifier)
         return ProductImage.objects.filter(product=product).order_by("id")
+
+    def create(self, request, *args, **kwargs):
+        product_identifier = self.kwargs["product_pk"]
+        get_product_or_404(product_identifier)
+        return super().create(request, *args, **kwargs)
+
+
+class ProductReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filterset_class = ReviewFilter
+    ordering_fields = ["rating", "created_at"]
+
+    def get_queryset(self):
+        product_identifier = self.kwargs["product_pk"]
+        product = get_product_or_404(product_identifier)
+        return Review.objects.filter(product=product)
 
     def create(self, request, *args, **kwargs):
         product_identifier = self.kwargs["product_pk"]
