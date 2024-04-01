16b5379d5eec86019cda1e485b39d09fe53242dc
Paw565pl <paw565pl@gmail.com>
2024-04-01 18:52:36 +0200

    feat(likes): create like and dislike model with generic foreign key

--- likes/models.py
+++ likes/models.py
@@ -1,12 +1,35 @@
-from django.db import models
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
 
 
 # Create your models here.
-class LikedItem(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+class LikeDislike(models.Model):
+    DISLIKE = -1
+    LIKE = 1
+
+    VOTES = ((DISLIKE, "Dislike"), (LIKE, "Like"))
+
+    vote = models.SmallIntegerField(verbose_name="Like/Dislike", choices=VOTES)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True
+    )
+
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["content_type", "object_id"]),
+        ]
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "content_type", "object_id"],
+                name="one_vote_per_user",
+            ),
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.user} {self.get_vote_display()} {self.content_type.model} {self.object_id}"
