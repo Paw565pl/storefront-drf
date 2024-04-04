4b34dfa91bbd9faef5bb0fcb34c3f7eac96c5f41
Paw565pl <paw565pl@gmail.com>
2024-04-04 14:32:11 +0200

    feat(likes): create method for getting content object id and updated put handler

--- likes/views.py
+++ likes/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.db.models import QuerySet
 from rest_framework import status
 from rest_framework.generics import get_object_or_404, GenericAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from core.exceptions import Conflict
@@ -17,20 +18,30 @@ class LikeDislikeViewSet(GenericAPIView):
 
     queryset = LikeDislike.objects.all()
     serializer_class = LikeDislikeSerializer
+    permission_classes = [IsAuthenticated]
 
     def dispatch(self, request, *args, **kwargs):
         if self.content_object_queryset is None:
             raise AttributeError("content_object_queryset must be set")
         return super().dispatch(request, *args, **kwargs)
 
+    def get_content_object_id(self, *args, **kwargs):
+        content_object_id = self.kwargs.get("pk")
+
+        if content_object_id is None:
+            raise AttributeError("you must include the object pk in the url path")
+
+        return content_object_id
+
     def get_object(self, *args, **kwargs):
-        object_id = self.kwargs["pk"]
+        content_object_id = self.get_content_object_id()
         content_type = ContentType.objects.get_for_model(
             self.content_object_queryset.model
         )
+
         return get_object_or_404(
             self.get_queryset(),
-            object_id=object_id,
+            object_id=content_object_id,
             content_type=content_type,
             user=self.request.user,
         )
@@ -38,19 +49,22 @@ class LikeDislikeViewSet(GenericAPIView):
     def get(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = self.get_serializer(instance)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
+        content_object_id = self.get_content_object_id()
         content_object = get_object_or_404(
-            self.content_object_queryset, pk=kwargs["pk"]
+            self.content_object_queryset, pk=content_object_id
         )
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
         try:
             self.get_queryset().create(
                 **serializer.validated_data,
                 user=request.user,
-                content_object=content_object
+                content_object=content_object,
             )
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         except IntegrityError:
@@ -60,14 +74,18 @@ class LikeDislikeViewSet(GenericAPIView):
             )
 
     def put(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-
         instance = self.get_object()
-        instance.vote = serializer.validated_data["vote"]
-        instance.save()
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        if getattr(instance, "_prefetched_objects_cache", None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
 
     def delete(self, request, *args, **kwargs):
         instance = self.get_object()
