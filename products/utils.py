472ffc40f78ff4986d2c29bcef6381600e79d3c9
Paw565pl <paw565pl@gmail.com>
2024-07-24 13:03:36 +0200

    refactor: add more precise exception clause

--- products/utils.py
+++ products/utils.py
@@ -6,12 +6,12 @@ from products.models import Product
 def get_product_or_404(product_identifier: int | str) -> Product | None:
     try:
         return Product.objects.get(pk=product_identifier)
-    except Exception:
+    except (Product.DoesNotExist, ValueError):
         pass
 
     try:
         return Product.objects.get(slug=product_identifier)
-    except Exception:
+    except (Product.DoesNotExist, ValueError):
         pass
 
     raise NotFound("No %s matches the given query." % Product._meta.object_name)
