bd8b806289fd482c8d5f9647ff239be1627fa9e3
Paw565pl <paw565pl@gmail.com>
2024-07-24 13:09:51 +0200

    refactor: simplify urls setup

--- storefront/urls.py
+++ storefront/urls.py
@@ -29,20 +29,27 @@ admin.site.index_title = "Admin"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth/", include("djoser.urls")),
-    path("api/auth/", include("djoser.urls.jwt")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger-ui/",
+        "schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/schema/redoc/",
+        "schema/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-    path("api/", include("products.urls")),
+    path(
+        "api/",
+        include(
+            [
+                path("", include("products.urls")),
+                path("auth/", include("djoser.urls")),
+                path("auth/", include("djoser.urls.jwt")),
+            ]
+        ),
+    ),
 ]
 
 if settings.DEBUG:
