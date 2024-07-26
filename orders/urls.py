e78fa9f020bd736f894ced2bc540fb6340c02210
Paw565pl <paw565pl@gmail.com>
2024-07-26 11:26:17 +0200

    feat(orders): create router for cart view set

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework_nested import routers
 
-from orders.views import CustomerAddressView
+from orders.views import CustomerAddressView, CartViewSet
+
+router = routers.DefaultRouter()
+router.register(r"carts", CartViewSet, basename="carts")
 
 urlpatterns = [
+    path("", include(router.urls)),
     path("orders/address", CustomerAddressView.as_view(), name="customer-address"),
 ]
