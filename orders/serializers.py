4c907595cafea188891dd12c60750012a9177396
Paw565pl <paw565pl@gmail.com>
2024-07-26 11:24:28 +0200

    feat(orders): create cart item serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from rest_framework.generics import get_object_or_404
 
-from orders.models import Customer, CustomerAddress
+from orders.models import Customer, CustomerAddress, CartItem
+from products.models import Product
+from products.serializers import SimpleProductSerializer
 
 
 class CustomerAddressSerializer(serializers.ModelSerializer):
@@ -32,3 +34,22 @@ class CustomerAddressSerializer(serializers.ModelSerializer):
         customer.save()
 
         return created_address
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ["id", "product_id", "product", "quantity", "total_price"]
+
+    product_id = serializers.IntegerField(write_only=True)
+
+    product = SimpleProductSerializer(read_only=True)
+    total_price = serializers.DecimalField(
+        read_only=True, max_digits=10, decimal_places=2
+    )
+
+    @staticmethod
+    def validate_product_id(product_id):
+        if not Product.objects.filter(id=product_id).exists():
+            raise serializers.ValidationError("Invalid product id.")
+        return product_id
