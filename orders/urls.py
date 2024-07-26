cfd51450e0bfe5379449f28bb422b7269e99219f
Paw565pl <paw565pl@gmail.com>
2024-07-26 14:51:54 +0200

    chore(orders): swap router to simple router

--- orders/urls.py
+++ orders/urls.py
@@ -3,10 +3,10 @@ from rest_framework_nested import routers
 
 from orders.views import CustomerAddressView, CartViewSet, CartItemViewSet
 
-router = routers.DefaultRouter()
+router = routers.SimpleRouter()
 router.register(r"carts", CartViewSet, basename="carts")
 
-cart_router = routers.NestedDefaultRouter(router, r"carts", lookup="cart")
+cart_router = routers.NestedSimpleRouter(router, r"carts", lookup="cart")
 cart_router.register(r"items", CartItemViewSet, basename="cart-items")
 
 urlpatterns = [
