7de718bf6ceaab7f603cd64b3dcf78ffa3396c2d
Paw565pl <paw565pl@gmail.com>
2024-07-26 10:55:20 +0200

    feat(orders): create customer address view

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.generics import get_object_or_404, RetrieveUpdateDestroyAPIView
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.permissions import IsAuthenticated
+
+from orders.models import CustomerAddress
+from orders.serializers import CustomerAddressSerializer
+
 
 # Create your views here.
+class CustomerAddressView(CreateModelMixin, RetrieveUpdateDestroyAPIView):
+    serializer_class = CustomerAddressSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return CustomerAddress.objects.filter(customer__user_id=self.request.user.id)
+
+    def get_object(self):
+        return get_object_or_404(
+            self.get_queryset(), customer__user_id=self.request.user.id
+        )
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
