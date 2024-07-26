c55e33e956eb91b25566b3777cac8851c0ccd38a
Paw565pl <paw565pl@gmail.com>
2024-07-26 14:50:32 +0200

    feat(orders): create nested router for cart item view set

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 
-from orders.views import CustomerAddressView, CartViewSet
+from orders.views import CustomerAddressView, CartViewSet, CartItemViewSet
 
 router = routers.DefaultRouter()
 router.register(r"carts", CartViewSet, basename="carts")
 
+cart_router = routers.NestedDefaultRouter(router, r"carts", lookup="cart")
+cart_router.register(r"items", CartItemViewSet, basename="cart-items")
+
 urlpatterns = [
-    path("", include(router.urls)),
     path("orders/address", CustomerAddressView.as_view(), name="customer-address"),
+    path("", include(router.urls)),
+    path("", include(cart_router.urls)),
 ]
