f0c4f09a4c8c7475b474e7d699f476d1bdce6afa
Paw565pl <paw565pl@gmail.com>
2024-07-29 12:37:47 +0200

    fix: create missing pytest fixtures

--- conftest.py
+++ conftest.py
@@ -24,8 +24,14 @@ def admin_api_client(api_client, django_user_model):
 
 
 @pytest.fixture
+def test_user(django_user_model):
+    user = baker.make(django_user_model)
+    return user
+
+
+@pytest.fixture
 def create_image_file():
-    def do_create_image_file():
+    def do_create_image_file() -> SimpleUploadedFile:
         file = SimpleUploadedFile(
             "test_image.jpeg",
             b"\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\x05\x04\x04\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b",
