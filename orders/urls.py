428365d7620407d439d88ee116aa57f149f3ba71
Paw565pl <paw565pl@gmail.com>
2024-08-04 16:59:31 +0200

    feat(orders): create router for order view set

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework_nested import routers
 
-from orders.views import CustomerAddressView, CartViewSet, CartItemViewSet
+from orders.views import CustomerAddressView, CartViewSet, CartItemViewSet, OrderViewSet
 
 router = routers.SimpleRouter()
 router.register(r"carts", CartViewSet, basename="carts")
+router.register(r"orders", OrderViewSet, basename="orders")
 
 cart_router = routers.NestedSimpleRouter(router, r"carts", lookup="cart")
 cart_router.register(r"items", CartItemViewSet, basename="cart-items")
