69771e5f0784bd02e56667d754c8679e20699e89
Paw565pl <paw565pl@gmail.com>
2024-08-05 21:14:07 +0200

    refactor(orders): simplify signals logic

--- orders/signals.py
+++ orders/signals.py
@@ -3,7 +3,7 @@ from django.db.models import Sum
 from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 
-from orders.models import Customer, CartItem, Cart, OrderItem, Order
+from orders.models import Customer, CartItem, OrderItem, Order, Cart
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -20,7 +20,12 @@ def calculate_item_total_price(sender, instance: CartItem | OrderItem, **kwargs)
 
 @receiver([post_save, post_delete], sender=CartItem)
 def update_cart_total_price(sender, instance: CartItem, **kwargs):
-    def do_update_cart_total_price():
+    origin = kwargs.get("origin")
+
+    # update total price if model is saved or directly deleted
+    model_save = origin is None
+    direct_model_delete = isinstance(origin, OrderItem)
+    if model_save or direct_model_delete:
         cart_id = instance.cart_id
         new_total_price = (
             CartItem.objects.filter(cart_id=cart_id)
@@ -30,24 +35,17 @@ def update_cart_total_price(sender, instance: CartItem, **kwargs):
         )
         Cart.objects.filter(id=cart_id).update(total_price=new_total_price)
 
-    origin = kwargs.get("origin")
-    if origin is None:  # update total price if signal is post_save
-        do_update_cart_total_price()
-    elif isinstance(
-        origin, CartItem
-    ):  # update total price if delete origin is CartItem
-        do_update_cart_total_price()
-
 
 @receiver([post_save, post_delete], sender=OrderItem)
 def update_order_total_price(sender, instance: OrderItem, **kwargs):
-    def do_update_order_total_price():
-        order_id = instance.order_id
-        new_item_total_price = instance.quantity * instance.product.unit_price
-        OrderItem.objects.filter(id=instance.id).update(
-            total_price=new_item_total_price
-        )
+    origin = kwargs.get("origin")
+    created = kwargs.get("created")
 
+    # update total price if model is updated or directly deleted
+    model_update = origin is None and not created
+    direct_model_delete = isinstance(origin, OrderItem)
+    if model_update or direct_model_delete:
+        order_id = instance.order_id
         new_total_price = (
             OrderItem.objects.filter(order_id=order_id)
             .aggregate(Sum("total_price"))
@@ -55,12 +53,3 @@ def update_order_total_price(sender, instance: OrderItem, **kwargs):
             or 0
         )
         Order.objects.filter(id=order_id).update(total_price=new_total_price)
-
-    origin = kwargs.get("origin")
-    created = kwargs.get("created")
-
-    # update total price only if model is updated or directly deleted
-    model_update = origin is None and not created
-    direct_model_delete = isinstance(origin, OrderItem)
-    if model_update or direct_model_delete:
-        do_update_order_total_price()
