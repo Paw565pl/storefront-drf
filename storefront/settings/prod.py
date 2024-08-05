061f850208d73d81d607bfc6e1a3fd1347a9fc70
Paw565pl <paw565pl@gmail.com>
2024-08-05 23:01:21 +0200

    chore: refactor prod settings using django environ

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,56 +1,44 @@
-# from os import environ
-#
-# from dj_database_url import config as db_config
-#
-# SECRET_KEY = environ["SECRET_KEY"]
-#
-# DEBUG = False
-#
-# ALLOWED_HOSTS = []
-#
-# DATABASES = {"default": db_config()}
-#
-# REDIS_URL = environ.get("REDIS_URL")
-#
-# CELERY_BROKER_URL = REDIS_URL
-#
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-#         "LOCATION": REDIS_URL,
-#         "TIMEOUT": 10 * 60,
-#     }
-# }
-#
-# STORAGES = {
-#     "staticfiles": {
-#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-#     },
-# }
-#
-# LOGGING = {
-#     "version": 1,
-#     "disable_existing_loggers": False,
-#     "handlers": {
-#         "console": {
-#             "class": "logging.StreamHandler",
-#         },
-#         "file": {
-#             "class": "logging.FileHandler",
-#             "filename": "general.log",
-#             "formatter": "verbose",
-#         },
-#     },
-#     "loggers": {
-#         "django": {
-#             "handlers": ["console", "file"],
-#             "level": environ.get("DJANGO_LOG_LEVEL", "INFO"),
-#         },
-#     },
-#     "formatters": {
-#         "verbose": {
-#             "format": "{asctime} ({levelname}) - {name} - {message}",
-#             "style": "{",
-#         }
-#     },
-# }
+import environ
+
+from .common import *  # noqa
+
+env = environ.Env(LOG_LEVEL=(str, "ERROR"))
+environ.Env.read_env(BASE_DIR / ".env")  # noqa: F405
+
+SECRET_KEY = env("SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+DATABASES = {
+    "default": env.db(),
+}
+
+CELERY_BROKER_URL = env.cache()
+
+CACHES = {"default": env.cache()}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+        "file": {
+            "class": "logging.FileHandler",
+            "filename": "general.log",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {"handlers": ["console", "file"], "level": env("LOG_LEVEL")},
+    },
+    "formatters": {
+        "verbose": {
+            "format": "{asctime} ({levelname}) - {name} - {message}",
+            "style": "{",
+        }
+    },
+}
