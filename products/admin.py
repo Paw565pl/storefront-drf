7249eb5f70bb2df7367c86aa7be0de7ae632adf8
Paw565pl <paw565pl@gmail.com>
2024-08-05 17:54:07 +0200

    refactor(products): admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,5 @@
-from typing import Any
-
 from django.contrib import admin, messages
 from django.db.models import QuerySet, Count, Q
-from django.http import HttpRequest
 from django.urls import reverse
 from django.utils.html import format_html
 from django.utils.http import urlencode
@@ -20,7 +17,7 @@ class InventoryFilter(admin.SimpleListFilter):
     def lookups(self, request, model_admin):
         return ("<10", "Low"), (">=10", "Ok")
 
-    def queryset(self, request, queryset: QuerySet[Any]):
+    def queryset(self, request, queryset: QuerySet):
         if self.value() == "<10":
             return queryset.filter(inventory__lt=10)
         elif self.value() == ">=10":
@@ -29,20 +26,21 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(Collection)
 class CollectionAdmin(admin.ModelAdmin):
+    readonly_fields = ["products_count"]
     search_fields = ["title__icontains"]
     list_display = ["title", "products_count"]
     list_per_page = 20
 
     @admin.display(ordering="products_count")
-    def products_count(self, collection):
+    def products_count(self, collection: Collection):
         url = (
             reverse("admin:products_product_changelist")
             + "?"
             + urlencode({"collection_id": str(collection.id)})
         )
-        return format_html("<a href='{}'>{}</a>", url, collection.products_count)
+        return format_html("<a href='{}'>{}</a>", url, collection.products_count)  # noqa
 
-    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+    def get_queryset(self, request):
         return super().get_queryset(request).annotate(products_count=Count("product"))
 
 
@@ -68,22 +66,65 @@ class ProductReviewInline(TabularInlinePaginated):
     extra = 1
 
 
+class PromotionFilter(admin.SimpleListFilter):
+    title = "Promotion discount"
+    parameter_name = "discount"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("0-19", "0-19 %"),
+            ("20-39", "20-39 %"),
+            ("40-59", "40-59 %"),
+            ("60-79", "60-79 %"),
+            ("80-100", "80-100 %"),
+        )
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == "0-19":
+            return queryset.filter(discount__lte=19)
+        elif self.value() == "20-39":
+            return queryset.filter(discount__range=(20, 39))
+        elif self.value() == "40-59":
+            return queryset.filter(discount__range=(40, 59))
+        elif self.value() == "60-79":
+            return queryset.filter(discount__range=(60, 79))
+        elif self.value() == "80-100":
+            return queryset.filter(discount__range=(80, 100))
+
+
+@admin.register(Promotion)
+class PromotionAdmin(admin.ModelAdmin):
+    readonly_fields = ["products_count"]
+    search_fields = ["name__icontains"]
+    list_per_page = 20
+    list_display = ["name", "discount", "products_count"]
+    list_filter = [PromotionFilter]
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(products_count=Count("product"))
+
+    @admin.display(ordering="products_count")
+    def products_count(self, promotion):
+        return promotion.products_count
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_per_page = 20
-    autocomplete_fields = ["collection", "promotions"]
-    search_fields = ["title__icontains"]
     readonly_fields = ["slug", "last_update", "likes_count", "dislikes_count"]
-    actions = ["clear_inventory"]
     inlines = [ProductImageInline, ProductReviewInline]
+    search_fields = ["title__icontains"]
+    autocomplete_fields = ["collection", "promotions"]
+    actions = ["clear_inventory"]
+    list_per_page = 20
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
-    list_filter = ["collection", "last_update", InventoryFilter]
+    list_filter = ["collection", "last_update", InventoryFilter, PromotionFilter]
     list_select_related = ["collection"]
 
     def get_queryset(self, request):
         return (
             super()
             .get_queryset(request)
+            .prefetch_related("promotions", "productimage_set")
             .annotate(
                 likes_count=Count("votes", filter=Q(votes__value=Vote.LIKE)),
                 dislikes_count=Count("votes", filter=Q(votes__value=Vote.DISLIKE)),
@@ -109,7 +150,7 @@ class ProductAdmin(admin.ModelAdmin):
         return product.dislikes_count
 
     @admin.action(description="Clear inventory")
-    def clear_inventory(self, request, queryset: QuerySet[Any]):
+    def clear_inventory(self, request, queryset: QuerySet):
         updated_count = queryset.update(inventory=0)
         self.message_user(
             request,
@@ -118,56 +159,14 @@ class ProductAdmin(admin.ModelAdmin):
         )
 
 
-class PromotionFilter(admin.SimpleListFilter):
-    title = "Promotion discount"
-    parameter_name = "discount"
-
-    def lookups(self, request, model_admin):
-        return (
-            ("0-19", "0-19 %"),
-            ("20-39", "20-39 %"),
-            ("40-59", "40-59 %"),
-            ("60-79", "60-79 %"),
-            ("80-100", "80-100 %"),
-        )
-
-    def queryset(self, request, queryset: QuerySet[Any]):
-        if self.value() == "0-19":
-            return queryset.filter(discount__lte=19)
-        elif self.value() == "20-39":
-            return queryset.filter(discount__range=(20, 39))
-        elif self.value() == "40-59":
-            return queryset.filter(discount__range=(40, 59))
-        elif self.value() == "60-79":
-            return queryset.filter(discount__range=(60, 79))
-        elif self.value() == "80-100":
-            return queryset.filter(discount__range=(80, 100))
-
-
-@admin.register(Promotion)
-class PromotionAdmin(admin.ModelAdmin):
-    list_per_page = 20
-    list_display = ["name", "discount", "products_count"]
-    readonly_fields = ["products_count"]
-    search_fields = ["name__icontains"]
-    list_filter = [PromotionFilter]
-
-    def get_queryset(self, request):
-        return super().get_queryset(request).annotate(products_count=Count("product"))
-
-    @admin.display(ordering="products_count")
-    def products_count(self, promotion):
-        return promotion.products_count
-
-
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
+    readonly_fields = ["author", "product", "likes_count", "dislikes_count"]
+    search_fields = ["author__username__icontains"]
     list_per_page = 20
     list_display = ["author", "product", "rating", "created_at"]
-    search_fields = ["author__username__icontains"]
     list_filter = ["created_at"]
-    readonly_fields = ["author", "product", "likes_count", "dislikes_count"]
-    list_select_related = ["product"]
+    list_select_related = ["product", "author"]
 
     def get_queryset(self, request):
         return (
