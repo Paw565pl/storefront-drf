63d377fbe61ba43998eab3391c195912543c6d7f
Paw565pl <paw565pl@gmail.com>
2024-08-06 09:46:48 +0200

    chore: set different directory for mock images for testing

--- storefront/settings/test.py
+++ storefront/settings/test.py
@@ -6,6 +6,8 @@ SECRET_KEY = "django-insecure-p84^d=98%mj4bvtgo7dn3d38djc*1cfm-!bb9=+@4@1m+xnbz0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+MEDIA_ROOT = BASE_DIR / "media/test"  # noqa: F405
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
